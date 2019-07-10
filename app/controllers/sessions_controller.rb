e59fb10c2aec7a828cde3c5b592d7fd9bd3f8df4
Gitanjali Mule <gitanjalidmule@gmail.com>
2019-07-10 16:56:27 -0500

    welcome and home page setup through sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,19 @@
 class SessionsController < ApplicationController
+
+	def home
+		if user_signed_in?
+			@user = current_user
+			render :home
+		else
+			redirect_to welcome_path
+		end
+	end
+
+	def welcome
+		if user_signed_in?
+			redirect_to home_path
+		else
+			render :welcome
+		end
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+	root "sessions#welcome"
+	get '/home', to: 'sessions#home', as: 'home'
   devise_for :users, :controllers => { registrations: 'registrations'}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
