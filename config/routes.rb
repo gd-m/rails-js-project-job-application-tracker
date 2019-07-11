979f0a093103171db62b5579dd32df41ec8b7417
Gitanjali Mule <gitanjalidmule@gmail.com>
2019-07-10 19:22:16 -0500

    added navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-	root "sessions#welcome"
+	root "sessions#welcome" , as: 'welcome'
 	get '/home', to: 'sessions#home', as: 'home'
   devise_for :users, :controllers => { registrations: 'registrations'}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
