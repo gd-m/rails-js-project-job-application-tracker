a07466cf2392a615b90f00b915121cf53966c880
Gitanjali Mule <gitanjalidmule@gmail.com>
2019-07-12 14:45:33 -0500

    generated job_application resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :job_applications
 	root "sessions#welcome" , as: 'welcome'
 	get '/home', to: 'sessions#home', as: 'home'
   devise_for :users, :controllers => { registrations: 'registrations'}
